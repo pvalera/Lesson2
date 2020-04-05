cb3077b9711c7f428e26494f4abb03cb47a8d2bf
Joshua J <pvalera@hotmail.com>
2020-04-05 00:15:40 -0700

    Made the robot draw four squares adding another for loop

--- src/examples/IntroToRobot.java
+++ src/examples/IntroToRobot.java
@@ -1,18 +1,29 @@
 package examples;
-
 import org.jointheleague.graphical.robot.Robot;
 
 public class IntroToRobot {
+	
 	public static void main(String[] args) {
 
 		Robot r2d2 = new Robot();
 		r2d2.penDown();
 		r2d2.setPenWidth(20);
 		r2d2.setSpeed(5);
-		for (int i = 0; i < 4; i++) {
-			r2d2.move(100);
+
+		// create four squares four times
+		for (int j = 0; j < 4; j++) {
+			
+			r2d2.setRandomPenColor();
+		
+			for (int i = 0; i < 4; i++) {
+				r2d2.move(100);
+				r2d2.turn(90);
+			}
+			
 			r2d2.turn(90);
+		
 		}
 
 	}
+	
 }
